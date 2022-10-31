f5b3534aed5ecbe91a4e97fe1133627038ee00de
noataso <112417805+noataso@users.noreply.github.com>
2022-11-01 00:26:48 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('mousemove',(e)=>{
+    const mouseX=e.clientX;
+    const mouseY=e.clientY;
+    gsap.to('.circle',{
+        x:mouseX,
+        y:mouseY,
+        stagger:-0.1,
+    });
+    gsap.set('.cursor',{
+        x:mouseX,
+        y:mouseY,
+    })
+})
